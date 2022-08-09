cf0cf78b7f36db54e0dffb6b27a8891a9df91761
nikan <Melliissa2022>
2022-08-09 15:20:08 +0500

    это вторая программа

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-a=int(input("расстояние между рядами"))
-b=int(input("расстояние между дырочками"))
-l=int(input("длина свободного конца шнурка"))
-n=int(input("кол-во дырочек в каждом ряду"))
-длина=(l*2)+(b*3)*2+4
-print(длина)
+x=int(input("1"))
+y=int(input("2"))
+z=int(input("3"))
+if x<y or x<z or x<y and x<z:
+    print("x")
+elif y<x or y<z or y<x and y<z:
+    print("y")
+elif z<x or z<y or z<x and z<y:
+    print("z")
